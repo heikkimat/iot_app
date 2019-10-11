629b04d47d697d0ba178420fe0a4806d03577f3e
Heikki <heikkimat@gmail.com>
2019-10-11 13:11:06 +0300

    config file added

---
+++ config.py
@@ -0,0 +1,2 @@
+local_postgres = 'postgresql://postgres:salasana@localhost/lexus'
+hero_postgres = 'postgres://vvpquppeobjuha:95dbf35c13ac823cfa28db1bfeeb2f0ddea2699de664134eecdfb15b58ed0b56@ec2-54-243-44-102.compute-1.amazonaws.com:5432/dccqj76920i04j'
